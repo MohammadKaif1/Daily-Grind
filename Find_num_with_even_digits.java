7814f2d6380e70b8a9cadc1070b68c2273bd78c8
MOHAMMAD KAIF <103691849+MohammadKaif1@users.noreply.github.com>
2022-07-28 11:45:48 +0530

    Add files via upload

---
+++ Find_num_with_even_digits.java
@@ -0,0 +1,36 @@
+public class Find_num_with_even_digits {
+    public static void main(String[] args) {
+        /*Given an array nums of integers,
+        return how many of them contain an even number of digits.
+        Example-->> INPUT-->[12,4,34,6,2345]
+                    OUTPUT-->3
+                    */
+        int [] arr= {12,34,555,2222,123444,77};
+        int result = findNumbers(arr);
+        System.out.println(result);
+
+
+    }
+    static int findNumbers(int []nums)
+    {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            int count =0;int digit=0;
+            while(nums[i]>0)
+            {
+                digit=nums[i]%10;
+                nums[i]/=10;
+                count++;
+
+            }
+            if(count%2==0)
+            {
+                ans++;
+            }
+
+
+
+        }
+        return ans;
+    }
+}
