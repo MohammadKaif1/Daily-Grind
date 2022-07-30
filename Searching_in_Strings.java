f4daaed3e1a93bad670b950f97b735798d677d93
MOHAMMAD KAIF <103691849+MohammadKaif1@users.noreply.github.com>
2022-07-30 22:10:43 +0530

    Update Searching_in_Strings.java

--- Searching_in_Strings.java
+++ Searching_in_Strings.java
@@ -6,10 +6,28 @@ public class Searching_in_Strings {
         String name = "kaifmalik";
         
         char target = 'm';
-       int ans= search(name,target);
+       boolean ans= search(name,target);
         System.out.println(ans);
+             System.out.println(Arrays.toString(name.toCharArray()));
+        //name.toCharArray() is a function which converts the string into array format which helps us to iterate easily,,
+
 
     }
+    //we can also use for each loop to iterate on every element of our string
+    static boolean  search2(String naam, char target) {
+        if (naam.length()==0) {
+            return false;
+        }
+        for (char ch: naam.toCharArray()) {
+            if(ch==target)
+            {
+                return true;
+            }
+
+        }
+        return false;
+
+        }
 
      static int  search(String naam, char target) {
         if (naam.length()==0)
