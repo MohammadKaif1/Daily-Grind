2f71aeedb6521ed2e7d6fe02ed1a931e60c8940a
MOHAMMAD KAIF <103691849+MohammadKaif1@users.noreply.github.com>
2022-08-10 23:43:43 +0530

    Update Find_num_with_even_digits.java

--- Find_num_with_even_digits.java
+++ Find_num_with_even_digits.java
@@ -13,7 +13,7 @@ public class Find_num_with_even_digits {
     }
     static int findNumber(int []nums)
     {
-        int ans=0;
+        int result=0;
         for(int i=0;i<nums.length;i++){
             int count =0;int digit=0;
             while(nums[i]>0)
@@ -25,12 +25,12 @@ public class Find_num_with_even_digits {
             }
             if(count%2==0)
             {
-                ans++;
+                result++;
             }
 
 
 
         }
-        return ans;
+        return result;
     }
 }
